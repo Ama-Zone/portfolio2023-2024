05be286ce9fc0f4a88aa398f72a6307e4a470e85
Ama-Zone <100293017+Ama-Zone@users.noreply.github.com>
2024-06-01 18:05:13 +0200

    page projets 04
    -ajout Js pour le slider
    -problème résolu pour les livres(button) qui étaient mal placé

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,24 @@
 "use strict";
 import { gsap } from "gsap";
 console.log(gsap.version);
+
+//page projets
+
+/* Slider slider*/
+
+
+        //slider btn 
+
+        let buttons = document.querySelectorAll(".slider__js");
+        for(let button of buttons){
+            button.addEventListener("click", (e)=>{
+                let tabs = document.querySelectorAll(".slider__switch");
+                for(let tab of tabs){
+                    tab.classList.add("slider--hidden");
+
+                    let tabId = e.currentTarget.getAttribute("data-tab");
+                    let currentTab = document.getElementById(tabId);
+                    currentTab.classList.remove("slider--hidden");
+		        }
+            });
+        }
